8f664a8175b030e248d2ce12d7b0ba45e0ef1792
Michal Sakal <sakalmichal50@gmail.com>
2025-05-27 23:08:31 +0200

    testy zacatek

---
+++ src/main/java/sample/bankingapp/Testovani/UzivatelTest.java
@@ -0,0 +1,30 @@
+package sample.bankingapp.Testovani;
+
+
+
+    import org.testng.annotations.Test;
+    import sample.bankingapp.Transakce;
+    import sample.bankingapp.Uzivatel;
+
+    import static org.junit.jupiter.api.Assertions.*;
+    import static org.testng.AssertJUnit.assertEquals;
+
+public class UzivatelTest {
+
+//        @Test
+//        void testZustatekPoJedneTransakci() {
+//            Uzivatel uzivatel = new Uzivatel("Karel");
+//            uzivatel.pridejTransakci(new Transakce("Káva", -50));
+//            assertEquals(-50, uzivatel.getZustatek());
+//        }
+//
+//        @Test
+//        void testViceTransakci() {
+//            Uzivatel u = new Uzivatel("Petr");
+//            u.pridejTransakci(new Transakce("Oběd", -100));
+//            u.pridejTransakci(new Transakce("Výplata", 1000));
+//            assertEquals(900, u.getZustatek());
+//        }
+    }
+
+
